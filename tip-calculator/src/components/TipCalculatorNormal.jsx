09eb55d8501047d1586c73ce8a2877c06a634648
Mark Dewey <mark.t.dewey@gmail.com>
2019-09-04 17:56:11 -0400

    Added problem component

---
+++ tip-calculator/src/components/TipCalculatorNormal.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+const TipCalculator = () => {
+  const [total, setTotal] = useState(0)
+  const [grandTotal, setGrandTotal] = useState(0)
+
+  const calculateTip = (total, percentage) => {
+    if (total) {
+      const tip = total * percentage
+      return tip
+    } else {
+      return 0
+    }
+  }
+
+  const selectTip = percentage => {
+    const tip = calculateTip(total, percentage)
+    const grandTotal = total + tip
+    setGrandTotal(grandTotal)
+  }
+
+  return (
+    <main>
+      <input
+        className="bill-total"
+        type="number"
+        value={total}
+        onChange={e => setTotal(e.target.value)}
+      />
+      <ul>
+        <ul>
+          <li>
+            15% - $<span id="fifteen-percent">{calculateTip(total, 0.15)}</span>
+            <button class="button-15" onClick={() => selectTip(0.15)}>
+              SELECT
+            </button>
+          </li>
+          <li>
+            18% - $<span id="eighteen-percent">1.50</span>
+            <button class="button-18">SELECT</button>
+          </li>
+          <li>
+            20% - $<span id="twenty-percent">2.00</span>
+            <button class="button-20">SELECT</button>
+          </li>
+        </ul>
+      </ul>
+      <h2>
+        total pre-tip: $<span id="total">{total || 0}</span>
+      </h2>
+      <h2>
+        tip: $<span id="total">1.00</span>
+      </h2>
+      <h2>
+        total: $<span id="total">{grandTotal}</span>
+      </h2>
+    </main>
+  )
+}
+
+export default TipCalculator
